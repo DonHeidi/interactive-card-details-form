05d67f2ef559f61f5bdfd65dd555d843b205bdc3
Sebastian Heitmann <me@sebastian-heitmann.dev>
2024-01-15 06:46:00 +0100

    fix(cardfront): fixed behavior in the case of month or year is undefined

--- src/components/preview/CardFront/index.tsx
+++ src/components/preview/CardFront/index.tsx
@@ -10,8 +10,8 @@ type CardDetailsFrontProps = {
 export default function CardFront({ cardNumber, cardholderName, expirationDate }: CardDetailsFrontProps) {
   const cardholderDisplay = cardholderName || 'Jane Appleseed'
   const numDisplay = createDisplayValue(16, cardNumber).replace(/(\d{4})(?=\d)/g, '$1 ')
-  const monthDisplay = createDisplayValue(2, expirationDate?.month.toString())
-  const yearDisplay = createDisplayValue(2, expirationDate?.year.toString())
+  const monthDisplay = createDisplayValue(2, expirationDate?.month?.toString())
+  const yearDisplay = createDisplayValue(2, expirationDate?.year?.toString())
   return (
     <div>
       <div>{cardholderDisplay}</div>
