184a25cd352785ab1e857128e69c7307789c7e86
Sebastian Heitmann <me@sebastian-heitmann.dev>
2024-03-02 09:09:04 +0100

    build: fixed merge issue
    Forgot to solve the merge conflict.

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
   base: '/interactive-card-details-form/',
-=======
-  base: '/interactive-credit-card-form/',
->>>>>>> 227181bf0afed00fb8923d4550841b51d46bb79d
 })
